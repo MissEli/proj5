dc3b03fbfbd1771dbd7716c8f0278dc267d33dd8
IvarWik <ivarwikander@gmail.com>
2021-11-15 14:27:03 +0100

    Update read_trim.py

--- read_trim.py
+++ read_trim.py
@@ -1,20 +1,34 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 12 11:33:44 2021
-
-@author: ivewi
-"""
-
 import numpy as np
+import matplotlib.pyplot as plt
 
-filename = input('Specify file to be read: ')
-file = np.loadtxt(filename, dtype=str,
-                  skiprows=12,max_rows=1000,usecols=(3))
+filenames = ['p577.txt','p1815.txt','a1400.txt','a1464.txt','a2050.txt','a4750.txt','t2730.txt']
+energies = [577000,1815000,1400000,1464000,2050000,4750000,2730000] #eV
+loss_avg = []
+loss_std = []
+for i, name in enumerate(filenames):
+    file = np.loadtxt(name, dtype=str,
+                      skiprows=12,max_rows=1000,usecols=(3))
 
-data =[]
-for line in file:
-    newline = line.replace(',','')
-    print(newline)
-    data.append(newline)
+    data =[]
+    for line in file:
+        newline = line.replace(',','0.')
+        data.append(float(newline))
+        
+    
+    energy = np.full(len(data),energies[i])
+    loss = np.subtract(energy,data)
+    loss_avg.append(-1*np.mean(loss))
+    loss_std.append(np.std(loss))
+    
+    x = range(len(data))
+    plt.figure()
+    plt.plot(x, data ,linestyle='None',marker='.')
+    bottom, top = plt.ylim()
+    plt.ylim(0,1.2*top)
+    plt.xlabel('Event number')
+    plt.ylabel('Energy loss [eV]')
 
+print(f'Average losses {loss_avg}')
+print(f'Standard deviations {loss_std}')
     
