151b8033d5df1970f56ee09bc2f0b929f4a0107c
MissEli <elizaveta.y@outlook.com>
2021-11-18 13:40:40 +0100

    particle data stored in separate files

--- proj_code.py
+++ proj_code.py
@@ -2,18 +2,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = open('protons_and_alphas_and_mcp.txt', 'r')
+f = open('protons_and_alphas_with_mcp.txt', 'r')
 
 # create vectors
-ev = {}
+ev = []
+p577 = []
+p1815 = []
+a1400 = []
+a1464 = []
+a2050 = []
+a4750 = []
+t2730 = []
+
 E = []
 T = []
 
+peaks = [16.83597726854288, 53.18815906929055, 40.330077483222645, 42.00658965579711, 59.38278971039864, 139.2813243716698, 79.63379111094523]
+sigma = [0.2567847993549146, 0.27959312391297864, 0.6725911179773683, 0.2934586147656381, 0.28921837117948007, 0.32791849633710735, 0.2785334352492435]
 
 i = 0 # count events
 
 for line in f:
-    # if i == 100:
+    # if i == 10000:
     #     break
     
     line = line.strip()
@@ -26,13 +36,27 @@ for line in f:
         #   event nr             PH                  ToF            MCP PH
         #ev[columns[1]] = float(columns[3]), float(columns[5]) , float(columns[7])
         
-        if float(columns[7]) > 10:
+        if float(columns[5]) > 0:
             try:
                 e = float(columns[3])
                 t = float(columns[5])
                 T.append(t)
                 E.append(e)
-                ev[columns[1]] = float(columns[3]), float(columns[5]) , float(columns[7])
+
+                if e > peaks[0]-3*sigma[0] and e < peaks[0]+3*sigma[0]:
+                    p577.append([e, t , float(columns[7])])
+                if e > peaks[1]-3*sigma[1] and e < peaks[1]+3*sigma[1]:
+                    p1815.append([e, t , float(columns[7])])
+                if e > peaks[2]-3*sigma[2] and e < 41.32:
+                    a1400.append([e, t , float(columns[7])])
+                if e> 41.32 and e < peaks[3]+3*sigma[3]:
+                    a1464.append([e, t , float(columns[7])])
+                if e > peaks[4]-3*sigma[4] and e < peaks[4]+3*sigma[4]:
+                    a2050.append([e, t , float(columns[7])])
+                if e > peaks[5]-3*sigma[5] and e < peaks[5]+3*sigma[5]:
+                    a4750.append([e, t , float(columns[7])])
+                if e > peaks[6]-3*sigma[6] and e < peaks[6]+3*sigma[6]:
+                    t2730.append([e, t , float(columns[7])])
             except IndexError as ie:    #Only needed to find last index
                 print("*** Index Error: ")
                 print(i)          
@@ -44,8 +68,10 @@ f.close()
 print(i)
 
 
-# with open('ec.txt', 'w') as f2:
-#     f2.write('# Events per channel')
+
+
+# with open('particles.txt', 'w') as f2:
+#     f2.write('# Particle, PH, ToF')
 #     for x in Ec:
 #         f2.write('\n')
 #         f2.write(str(x)) 
@@ -62,17 +88,38 @@ print('done')
 
 
 # np.histogram(T, bins=10, range=None, normed=None, weights=None, density=None)
-plt.hist(E,bins = 2000, range=(0,200))
-plt.xlabel('energy channel')
-plt.ylabel('events')
-plt.show()
-
-# plt.plot(E,T,'.')
-# plt.xlabel('pulse height')
-# plt.ylabel('delta T [ns]')
+# plt.hist(E,bins = 2000, range=(0,200))
+# plt.xlabel('energy channel')
+# plt.ylabel('events')
 # plt.show()
 
-# Look at each event 
+plt.plot(E,T,'.')
+plt.xlabel('pulse height')
+plt.ylabel('delta T [ns]')
+# plt.show()
+
+# Look at each event separately
 # print('(PH, ToF)')
-# for n in ev:
-#     print(ev[n])
+ToF = []
+PH = []
+ev = [p577,p1815,a1400,a1464,a2050,a4750,t2730]
+for list in ev:
+    for x in list:
+        ToF.append(x[1])
+        PH.append(x[0])
+
+
+plt.plot(PH,ToF,'r.')
+plt.show()
+
+fname = ['p577.txt','p1815.txt','a1400.txt','a1464.txt','a2050.txt','a4750.txt','t2730.txt','allp.txt']
+
+for i,list in enumerate(ev):
+    print(i)
+    with open(fname[i], 'w+') as file:
+        file.write('#'+fname[i]+', PH, ToF, MCP PH')
+        for x in list:
+            file.write('\n')
+            file.write(str(x))
+
+
