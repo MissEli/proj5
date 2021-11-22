f09bc8813eaf8ec05a5ba38671ccd8e8e09899c2
MissEli <elizaveta.y@outlook.com>
2021-11-22 15:48:05 +0100

    new files for each particle

--- proj_code.py
+++ proj_code.py
@@ -16,6 +16,7 @@ t2730 = []
 
 E = []
 T = []
+MCP = []
 
 peaks = [16.83597726854288, 53.18815906929055, 40.330077483222645, 42.00658965579711, 59.38278971039864, 139.2813243716698, 79.63379111094523]
 sigma = [0.2567847993549146, 0.27959312391297864, 0.6725911179773683, 0.2934586147656381, 0.28921837117948007, 0.32791849633710735, 0.2785334352492435]
@@ -27,36 +28,39 @@ for line in f:
     #     break
     
     line = line.strip()
-    # print(repr(line))
     columns = line.split()
     
     
     if (i >= 3) and (i < 19589):
-        # save each event into a dictionary
+        # save each event into an array
         #   event nr             PH                  ToF            MCP PH
-        #ev[columns[1]] = float(columns[3]), float(columns[5]) , float(columns[7])
+        #ev[columns[1]], float(columns[3]), float(columns[5]) , float(columns[7])
         
-        if float(columns[5]) > 0:
-            try:
-                e = float(columns[3])
-                t = float(columns[5])
-                T.append(t)
-                E.append(e)
+        n = float(columns[1])
+        e = float(columns[3])
+        t = float(columns[5])
+        mcp = float(columns[7])
+        T.append(t)
+        E.append(e)
+        MCP.append(mcp)
 
-                if e > peaks[0]-3*sigma[0] and e < peaks[0]+3*sigma[0]:
-                    p577.append([e, t , float(columns[7])])
-                if e > peaks[1]-3*sigma[1] and e < peaks[1]+3*sigma[1]:
-                    p1815.append([e, t , float(columns[7])])
-                if e > peaks[2]-3*sigma[2] and e < 41.32:
-                    a1400.append([e, t , float(columns[7])])
-                if e> 41.32 and e < peaks[3]+3*sigma[3]:
-                    a1464.append([e, t , float(columns[7])])
-                if e > peaks[4]-3*sigma[4] and e < peaks[4]+3*sigma[4]:
-                    a2050.append([e, t , float(columns[7])])
-                if e > peaks[5]-3*sigma[5] and e < peaks[5]+3*sigma[5]:
-                    a4750.append([e, t , float(columns[7])])
-                if e > peaks[6]-3*sigma[6] and e < peaks[6]+3*sigma[6]:
-                    t2730.append([e, t , float(columns[7])])
+        if float(columns[7]) > 10:
+            try:
+                
+                if n<1740:
+                    p577.append([e, t , mcp])
+                if n>1800 and n<4340:
+                    p1815.append([e, t , mcp])
+                if 4660<n and n<7730:
+                    a1400.append([e, t , mcp])
+                if 7760<n and n<10720:
+                    a1464.append([e, t , mcp])
+                if 10760<n and n<13630:
+                    a2050.append([e, t , mcp])
+                if 13670<n and n<17150:
+                    a4750.append([e, t , mcp])
+                if n>17170:
+                    t2730.append([e, t , mcp])
             except IndexError as ie:    #Only needed to find last index
                 print("*** Index Error: ")
                 print(i)          
@@ -64,6 +68,7 @@ for line in f:
         print(repr(line)) 
     
     i += 1
+
 f.close()
 print(i)
 
@@ -77,43 +82,49 @@ print('done')
 # plt.ylabel('energy channel')
 # plt.show()
 
-# np.histogram(T, bins=10, range=None, normed=None, weights=None, density=None)
+# histogram
 # plt.hist(E,bins = 2000, range=(0,200))
 # plt.xlabel('energy channel')
 # plt.ylabel('events')
 # plt.show()
 
-# plt.plot(E,T,'.')
-# plt.xlabel('pulse height')
-# plt.ylabel('delta T [ns]')
+# plt.plot(T,MCP,'.')
+# plt.xlabel('delta T [ns]')
+# plt.ylabel('MCP pulse height')
+
 # plt.show()
 
 
 # Look at each event separately
-# print('(PH, ToF)')
 ToF = []
 PH = []
 ev = [p577,p1815,a1400,a1464,a2050,a4750,t2730]
-i=1
+m= 0
 for list in ev:
     for x in list:
         ToF.append(x[1])
         PH.append(x[0])
-    plt.figure(i)
-    plt.hist(ToF,bins=100, range=(60,100))
-    plt.xlabel('tof')
-    
-    i+=1
+        m+=1
+    # plt.figure(i)
+    # plt.hist(ToF,bins=100)
+    # plt.xlabel('tof')
 
-plt.show()
-# plt.plot(PH,ToF,'r.')
+
+print(m)
+
 # plt.show()
+plt.plot(E,T,'b.')
+plt.xlabel('PH')
+plt.ylabel('t')
+plt.plot(PH,ToF,'r.')
+plt.legend(['all data','peak data'])
+plt.show()
 
 # Save to text-file
 fname = ['p577.txt','p1815.txt','a1400.txt','a1464.txt','a2050.txt','a4750.txt','t2730.txt','allp.txt']
 
 for i,list in enumerate(ev):
-    print(i)
+    # print(i)
     with open(fname[i], 'w+') as file:
         file.write('#'+fname[i]+', PH, ToF, MCP PH')
         for x in list:
